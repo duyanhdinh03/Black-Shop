05edf3c18ecaf573ea19eb5c8399b53d5b9fcd3d
dduya171 <duyanhk94lc@gmail.com>
2024-11-04 23:24:05 +0700

    Backend generate

---
+++ Backend/hmshop/src/main/java/com/ivandang/hmshop/HmshopApplication.java
@@ -0,0 +1,13 @@
+package com.ivandang.hmshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmshopApplication.class, args);
+	}
+
+}
---
+++ Backend/hmshop/src/test/java/com/ivandang/hmshop/HmshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivandang.hmshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
