b4f89d3a7bcadba6898bc6f49fba396043144fe0
dduya171 <duyanhk94lc@gmail.com>
2024-11-06 01:01:14 +0700

    regenerate

--- Frontend/hm-shop/src/app/app.component.spec.ts
+++ Frontend/hm-shop/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'hmshop-frontend' title`, () => {
+  it(`should have the 'hm-shop' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hmshop-frontend');
+    expect(app.title).toEqual('hm-shop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hmshop-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hm-shop');
   });
 });
