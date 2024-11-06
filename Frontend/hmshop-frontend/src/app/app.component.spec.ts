5eeed67a7cbbf61d23e74873e6f63a7ce54746ab
dduya171 <duyanhk94lc@gmail.com>
2024-11-07 00:45:40 +0700

    downgrade angular version 16

--- Frontend/hmshop-frontend/src/app/app.component.spec.ts
+++ Frontend/hmshop-frontend/src/app/app.component.spec.ts
@@ -2,11 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'hm-shop' title`, () => {
+  it(`should have as title 'hmshop-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hm-shop');
+    expect(app.title).toEqual('hmshop-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hm-shop');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('hmshop-frontend app is running!');
   });
 });
