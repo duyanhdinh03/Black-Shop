9fbd43810f70838dc1ca2951d8920b104386f9e5
dduya171 <duyanhk94lc@gmail.com>
2024-11-08 22:43:16 +0700

    FE : Call login API and get JWT token from BE Spring

--- Frontend/hmshop-fe/src/app/app.module.ts
+++ Frontend/hmshop-fe/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { OrderComponent } from './order/order.component';
-import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { DetailProductComponent } from './detail-product/detail-product.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { OrderComponent } from './components/order/order.component';
+import { OrderDetailComponent } from './components/order-confirm/order.detail.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DetailProductComponent } from './components/detail-product/detail-product.component';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import {
+  HttpClientModule,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http';
+import {TokenInterceptor} from './interceptors/token.interceptor'
+
 
 @NgModule({
   declarations: [
@@ -16,23 +24,34 @@ import { FormsModule } from '@angular/forms';
     HeaderComponent,
     FooterComponent,
     OrderComponent,
-    OrderConfirmComponent,
+    OrderDetailComponent,
     LoginComponent,
     RegisterComponent,
-    DetailProductComponent
+    DetailProductComponent,
+    // AppComponent
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    //AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [
+    // AppComponent
     // HomeComponent,
-    // OrderComponent,
-    // OrderConfirmComponent,
-    //LoginComponent,
-    RegisterComponent,
     //DetailProductComponent,
+    // OrderComponent,
+    OrderDetailComponent,
+    //LoginComponent,
+    // RegisterComponent
   ]
 })
 export class AppModule { }
